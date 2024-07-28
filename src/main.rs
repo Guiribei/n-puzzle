e64770b9b158315b788cc5cac1969b92c70ae713
Guilherme Ribeiro <guilherme_f_ribeiro@hotmail.com>
2024-07-28 19:18:47 -0300

    Add basic file IO

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("hello, seamen")
+use std::{fs::File, io::{BufRead, BufReader, Read}};
+
+fn main() -> std::io::Result<()> {
+    let file = File::open("puzzles/puzzle3x3.solvable")?;
+    let mut buf_reader = BufReader::new(file);
+    let mut contents = String::new();
+    buf_reader.read_to_string(&mut contents);
+    println!("{contents}");
+    Ok(())
 }
